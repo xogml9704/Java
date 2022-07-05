c3a64a6a4f777903e30b43f8c15967109933215f
KIM TAE HEE <xogml9704@naver.com>
2022-07-05 23:04:11 +0900

    practice

---
+++ Number.java
@@ -0,0 +1,18 @@
+public class Number {
+    public static void main(String[] args) {
+        // 자바에서는 따옴표가 없는 숫자로 인식된다.
+        System.out.println(1+2);
+        // 결과 : 3
+
+        System.out.println(1.2+1.3);
+        // 결과: 2.5
+        
+        //곱하기를 할 때는 *(에스터리크스, Asterisk)를 사용한다.
+        System.out.println(2*5);
+        // 결과 : 10
+
+        // 나누기를 할 때는 /(슬래쉬, slash)를 사용한다.
+        System.out.println(6/2);
+        // 결과 : 3
+    }
+}
