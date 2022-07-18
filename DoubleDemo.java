1065b04362b16288398032df38804ccb33be93ae
KIM TAE HEE <xogml9704@naver.com>
2022-07-18 22:17:07 +0900

    practice

---
+++ DoubleDemo.java
@@ -0,0 +1,9 @@
+public class DoubleDemo {
+    public static void main (String[] args) {
+        double a; // double = 실수
+        a = 1.1;
+
+        System.out.println(a+1.1);
+    }
+    
+}
--- Variable.java
+++ Variable.java
@@ -3,7 +3,9 @@ public class Variable {
         // 변수
         // 영어로는 Variable로 (문자나 숫자 같은) 데이터를 담는 컨테이너다.
 
-        int a;
+        int a; // 선언
+        // int = 데이터 형식(int = 숫자). a = 변수의 이름
+
         a = 1;
         System.out.println(a+1); // 2
         
