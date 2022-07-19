18b77f3e846e3dd1b6bdc57a708ccc185c995c11
KIM TAE HEE <xogml9704@naver.com>
2022-07-19 23:43:24 +0900

    practice

---
+++ StringDemo.java
@@ -0,0 +1,15 @@
+public class StringDemo {
+    public static void main (String[] args) {
+        // String first;
+        // first = "coding";
+
+        String first = "Coding"; // 한줄로 해결 가능
+
+        String a, b;
+        a = "coding";
+        b = "everybody";
+
+        System.out.println(a+b);
+    }
+    
+}
