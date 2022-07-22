67a33936e9e3827c058e85be9cfac74af97cdb86
KIM TAE HEE <xogml9704@naver.com>
2022-07-22 23:55:56 +0900

    practice

---
+++ 주석과세미콜론.java
@@ -0,0 +1,33 @@
+public class 주석과세미콜론 {
+    public static void main(String[] args) {
+        // 주석(comment)은 로직에 대한 설명이나 코드를 비활성화 할 때 사용한다.
+        // 주석은 프로그래밍적으로 해석되지 않는다.
+
+        // 한줄 주석 할때
+
+        /*
+         *  여러줄
+         *  주석 처리 할떄
+         */
+
+        /** 로 시작하는 주석은 JavaDoc 주석이라고 해서 자바의 문서를 만들 때 사용한다.
+         * 
+         */
+
+        // 세미콜론
+        // 세미콜론은 문장(statement)의 끝을 의미한다.
+        // 자바에서는 문장의 끝에 세미콜론을 사용하지 않으면 컴파일
+        // 에러가 발생한다.
+
+        aValue = 8933.246;
+
+        aValue++;
+
+        System.out.println("Hello World");
+
+        Bicycle myBike = new Bicycle();
+
+        // 세미콜론을 이용하면 여러개의 문장을 한줄에 표현할 수 있다.
+        int a = 100; double b = 10.1;
+    }
+}
