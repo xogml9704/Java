5e05357ef8cb01bf8d989ec56c51685d8c3f3c7f
KIM TAE HEE <xogml9704@naver.com>
2022-07-29 11:47:22 +0900

    practice

---
+++ 데이터타입/data.java
@@ -0,0 +1,18 @@
+package 데이터타입;
+
+public class data {
+    public static void main(String[] args) {
+        /*
+         * 데이터의 크기
+         * 8 bit (비트) : 1 byte
+         * 1024 byte (바이트) : 1 kilobyte
+         * 1024 kilobyte (킬로바이트) : 1 megabyte
+         * 1024 megabyte (메가바이트) : 1 gigabyte
+         * 1024 gigabyte (기가바이트) : 1 terabyte
+         * 1024 terabyte (테라바이트) : 1 petabyte
+         * 1024 petabyte (페타바이트) : 1 exabyte
+         * 1024 exabyte (엑사바이트) : 1 zettabyte
+         */
+        
+    }
+}
