85226fb13e96a7dc62f038f1b8381d2e2ea89ec9
KIM TAE HEE <xogml9704@naver.com>
2022-09-04 13:57:16 +0900

    practcie

--- baekjoon/b2439/Main.java
+++ baekjoon/b2439/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		for(int i=0; i<5; i++) {
 			for(int j=5; j<i; j--) {
-				System.out.print();
+				System.out.print("");
 			}
 			for(int j=0; j<5; j++) {
 				System.out.println("*");
