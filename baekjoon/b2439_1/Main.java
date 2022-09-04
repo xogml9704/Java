291a031d3f611faa8c69f8623f06b08cbcdee03c
KIM TAE HEE <xogml9704@naver.com>
2022-09-04 13:11:29 +0900

    practcie

--- baekjoon/b2439_1/Main.java
+++ baekjoon/b2439_1/Main.java
@@ -1,11 +1,11 @@
-package baekjoon.b2439;
+package baekjoon.b2439_1;
 
 public class Main {
 
 	public static void main(String[] args) {
 		for(int i=0; i<5; i++) {
 			for(int j=5; j<i; j--) {
-				System.out.print();
+				System.out.print(" ");
 			}
 			for(int j=0; j<5; j++) {
 				System.out.println("*");
--- memo/주석과세미콜론.java
+++ memo/주석과세미콜론.java
@@ -20,13 +20,13 @@ public class 주석과세미콜론 {
         // 자바에서는 문장의 끝에 세미콜론을 사용하지 않으면 컴파일
         // 에러가 발생한다.
 
-        aValue = 8933.246;
+        // aValue = 8933.246;
 
-        aValue++;
+        // aValue++;
 
         System.out.println("Hello World~~~~");
 
-        Bicycle myBike = new Bicycle();
+        // Bicycle myBike = new Bicycle();
 
         // 세미콜론을 이용하면 여러개의 문장을 한줄에 표현할 수 있다.
         int a = 100; double b = 10.1111;
