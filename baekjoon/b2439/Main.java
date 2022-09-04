03f0d8608ec174cb24eac7fb12f0f17b571b8279
KIM TAE HEE <xogml9704@naver.com>
2022-09-04 14:42:29 +0900

    practcie

--- baekjoon/b2439/Main.java
+++ baekjoon/b2439/Main.java
@@ -1,14 +1,20 @@
 package baekjoon.b2439;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
 	public static void main(String[] args) {
-		for(int i=0; i<5; i++) {
-			for(int j=5; j<i; j--) {
-				System.out.print("");
+
+
+		Scanner sc = new Scanner(System.in);
+		int a = Integer.parseInt(sc.nextLine());	
+
+		for(int i=0; i<a; i++) {
+			for(int j=a-1; j>i; j--) {
+				System.out.print(" ");
 			}
-			for(int j=0; j<5; j++) {
-				System.out.println("*");
+			for(int j=0; j<=i; j++) {
+				System.out.print("*");
 			}
 			System.out.println();
 		}
