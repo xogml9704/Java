aacf022123238c2623eff0c242bef61bdf90a6e3
KIM TAE HEE <xogml9704@naver.com>
2022-09-05 00:43:45 +0900

    practcie

---
+++ baekjoon/b10952/Main.java
@@ -0,0 +1,13 @@
+package baekjoon.b10952;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while(true) {
+            
+        }
+    }
+}
