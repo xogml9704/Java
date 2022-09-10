8d72104f348155f7b71e13ec89518b55ef5a9715
KIM TAE HEE <xogml9704@naver.com>
2022-09-10 13:52:20 +0900

    practcie

--- baekjoon/b10951/Main.java
+++ baekjoon/b10951/Main.java
@@ -1,5 +1,17 @@
 package baekjoon.b10951;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		while(sc.hasNextInt()) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			System.out.println(a+b);
+			
+		}
+		sc.close();
+	}
 }
---
+++ baekjoon/b1110/Main.java
@@ -0,0 +1,8 @@
+package baekjoon.b1110;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		
+	}
+}
