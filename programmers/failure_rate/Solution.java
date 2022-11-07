3b7232b94c10c427614e69cbc657717afbc72347
KIM TAE HEE <xogml9704@naver.com>
2022-11-07 22:52:40 +0900

    practice

--- programmers/failure_rate/Solution.java
+++ programmers/failure_rate/Solution.java
@@ -44,9 +44,6 @@ public class Solution {
         for(int i=0; i<sum.length; i++) {
             sum_arr[i] = (int) sum[i][0];
         }
-        for (int i=0; i<sumlength; i++) {
-            System.out.println(i);
-            System.out.println(i);
-        }
+
     }
 }
